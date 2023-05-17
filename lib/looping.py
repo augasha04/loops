f0d0def13a7a4d409ec0cbf6ed7ea199c2899819
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-17 08:58:50 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 1
+    while i < 11:
+        print(i)
+        i += 1
+    else:
+        print("Happy New Year")
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x*x for x in int_list]
+print(square_integers([1, 2, 3]))
 
-def fizzbuzz():
-    # code goes here!
-    pass
+
+def fizzbuzz(num):
+   i = 1
+   while i < 101:
+       if i % 3 == 0 and i % 5 == 0:
+           print("FizzBuzz")
+       elif i % 3 == 0:
+           print("Fizz")
+       elif i % 5 == 0:
+           print("Buzz")
+       else:
+           print(num)
+fizzbuzz()
